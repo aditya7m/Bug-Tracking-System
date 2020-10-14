8fb26c79475e17bf5a6529d1be7664ac1baf9b09
aditya7m <maheshwariaditya5555@gmail.com>
2020-10-14 23:07:21 +0530

    Updates

---
+++ precache-manifest.633f80fb8f27da6fbdb1686ce2a87e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645fa135197444f7853074cc9093c364",
+    "url": "/%7BBug-Tracking-System%7D/index.html"
+  },
+  {
+    "revision": "8f0fb92179a4215a4973",
+    "url": "/%7BBug-Tracking-System%7D/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "a8203dec893fe35b4f15",
+    "url": "/%7BBug-Tracking-System%7D/static/css/main.6bb02050.chunk.css"
+  },
+  {
+    "revision": "8f0fb92179a4215a4973",
+    "url": "/%7BBug-Tracking-System%7D/static/js/2.467347f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/%7BBug-Tracking-System%7D/static/js/2.467347f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8203dec893fe35b4f15",
+    "url": "/%7BBug-Tracking-System%7D/static/js/main.b8a76227.chunk.js"
+  },
+  {
+    "revision": "838630bb460d6faa627d",
+    "url": "/%7BBug-Tracking-System%7D/static/js/runtime-main.fde376d8.js"
+  }
+]);
